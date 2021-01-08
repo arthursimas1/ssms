df0519081eb3f0866b815faa2d08bd26e29da510
Arthur Simas <arthursimas1@gmail.com>
2021-01-07 21:54:22 -0300

    networking.py documented

--- networking.py
+++ networking.py
@@ -7,22 +7,41 @@ Address = Tuple[str, int]
 
 
 class Connection:
+    """
+    Both methods are static, that way we do not need to initialize any variable into our class.
+    With Static Methods both functions become linked to the class, not the object.
+    """
+
     @staticmethod
-    def send(conn, msg):
+    def send(conn: socket.socket, msg: bytes) -> None:
+        # Total sent bytes counter, used to determine
+        # whether the message has been sent successfully or not
         total_sent = 0
 
+        # Keeps sending message bytes until there's none to send
         while total_sent < len(msg):
+            # .send is a socket's function. Used to send data through a TCP based socket.
+            # [init:] = it skips init bytes of the message
             bytes_sent = conn.send(msg[total_sent:])
+
+            # If 0 bytes was sent, then there's a problem
             if bytes_sent == 0:
                 raise RuntimeError('socket connection broken')
             total_sent += bytes_sent
 
     @staticmethod
-    def receive(conn, msg_size):
+    def receive(conn: socket.socket, msg_size: int) -> bytes:
+        # Variable to store the message. It's initialized to an empty byte string.
         msg = b''
 
+        # Keeps receiving expected message bytes until there's none to receive
         while len(msg) < msg_size:
+
+            # .recv is a socket's function. Used to recovery data through a TCP based socket.
+            # If the message has more than 2048, it's broken into parts
             chunk = conn.recv(min(msg_size - len(msg), 2048))
+
+            # If 0 bytes was received, then there's a problem
             if chunk == b'':
                 raise RuntimeError('socket connection broken')
             msg += chunk
@@ -32,7 +51,8 @@ class Connection:
 
 class Server:
     """
-    Por padrão, cria um servidor ouvindo no endereço indicado, usando o protocolo TCP
+    By default, the server will be created into a given address, using the TCP protocol.
+    The shared key used by the cryptography is also passed.
     """
     def __init__(self, addr: Address, key: bytes) -> None:
         self.addr: Address = addr
@@ -43,16 +63,24 @@ class Server:
         self.dest_id: int = None
         self._message: bytes = None
 
+    """
+    Creates a socket with the AF_INET protocol (provides IPv4 support) and
+    the TCP protocol (SOCK_STREAM), then starts a server.
+    """
     def start(self) -> None:
-        # função de conveniência cujo cria um socket da família de protocolos AF_INET, cujo suporta IPv4,
-        # usando o protocolo do tipo SOCK_STREAM, ou seja, TCP
         self.sock = socket.create_server(address=self.addr, family=socket.AF_INET)
 
+    """
+    Accepts a connection and returns the client address. 
+    """
     def wait_for_connection(self) -> Address:
         self.conn, client_address = self.sock.accept()
 
         return client_address
 
+    """
+    Receives and stores all the data.
+    """
     def receive(self) -> None:
         receive = protocol.Receive(self.conn, self.key)
         receive.process()
@@ -60,12 +88,18 @@ class Server:
         self.dest_id = receive.dest_id
         self._message = receive.message
 
+    """
+    Message getter
+    """
     @property
     def message(self) -> bytes:
         if self._message is None:
-            raise KeyError('message not ready')
+            raise RuntimeError('message not ready')
         return self._message
 
+    """
+    Closes the socket and the connection to the client.
+    """
     def close(self) -> None:
         if self.conn:
             self.conn.close()
@@ -76,7 +110,8 @@ class Server:
 
 class Client:
     """
-    Por padrão, conecta a um servidor no endereço indicado, usando o protocolo TCP
+    By default, the server will connect into a specific address, using the TCP protocol.
+    The shared key used by the cryptography is also passed.
     """
     def __init__(self, addr: Address, source_id: int, dest_id: int, key: bytes, algorithm: str, pkcs5: bool) -> None:
         self.addr: Address = addr
@@ -87,24 +122,38 @@ class Client:
         self.pkcs5: bool = pkcs5
         self.conn: socket.socket = None
 
+    """
+    Connects to a server given the parameters.
+    """
     def connect(self) -> None:
-        # função de conveniência cujo se conecta a um servidor existente
         self.conn = socket.create_connection(address=self.addr)
 
+    """
+    Sends a message to the client in a given connection, from a source id,
+    to a destination id, using a given cryptography key, cryptography algorithm and an optional PKCS5 padding. 
+    """
     def send(self, msg: bytes) -> None:
         send = protocol.Send(self.conn, self.key, self.source_id, self.dest_id, self.algorithm, self.pkcs5, msg)
         send.process()
 
+    """
+    Closes the connection to the server.
+    """
     def close(self) -> None:
         if self.conn:
             self.conn.close()
 
 
 def open_server(addr: Address, key: bytes) -> None:
+    """
+    Primary function used to open the server and receive a message.
+    """
+
+    # Creates a server listening in addr, and with a shared key
     s = Server(addr, key)
 
     try:
-        s.start()
+        s.start()  # Start listening to connections
         print(f'server listening on {addr}')
     except ConnectionRefusedError:
         print(f'couldn\'t start server on {addr}.')
@@ -112,11 +161,13 @@ def open_server(addr: Address, key: bytes) -> None:
 
     try:
         print('waiting for a connection... press Ctrl + C to interrupt')
-        client_addr = s.wait_for_connection()
+        client_addr = s.wait_for_connection()  # Waits for a connection to establish
         print(f'connection accepted from {client_addr}')
 
+        # Starts receiving the message
         s.receive()
 
+        # Gets source id, destination id, and the message itself
         source = s.source_id
         dest = s.dest_id
         message = s.message
@@ -136,10 +187,17 @@ def open_server(addr: Address, key: bytes) -> None:
 
 
 def open_client(addr: Address, source_id: int, dest_id: int, key: bytes, algorithm: str, pkcs5: bool) -> None:
+    """
+    Primary function used to connect the a server and send a message.
+    """
+
+    # Creates a client with a given source id, destination id, shared key,
+    # cryptographic algorithm and pkcs5 padding option.
+    # It will try to connect to a server in addr.
     c = Client(addr, source_id, dest_id, key, algorithm, pkcs5)
 
     try:
-        c.connect()
+        c.connect()  # Tries to connect to the server
     except ConnectionRefusedError:
         print('couldn\'t connect to server.')
         return
@@ -149,6 +207,8 @@ def open_client(addr: Address, source_id: int, dest_id: int, key: bytes, algorit
     try:
         print('type your message (reading until EOF, Ctrl + D in some terminals)... press Ctrl + C to interrupt')
 
+        # Reads stdin. Strip whitespaces at the beginning and the end fo the string to disallow empty messages
+        # which can lead to 0 bytes being sent and breaking the socket
         terminal_input = ''
         while terminal_input.strip() == '':
             terminal_input = stdin.read()
@@ -162,6 +222,7 @@ def open_client(addr: Address, source_id: int, dest_id: int, key: bytes, algorit
         return
 
     try:
+        # Sends to the server the read message
         c.send(terminal_input.encode('utf-8'))
     except Exception as e:
         print(f'some error occurred: {e}')
